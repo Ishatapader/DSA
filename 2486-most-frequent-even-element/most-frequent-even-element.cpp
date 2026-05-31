b0e2c306b7f6b646e1b637c019aba1ca1605b61c
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-05-31 10:51:54 +0530

    Time: 55 ms (9.18%) | Memory: 133.1 MB (5.05%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(100001, 0);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                freq[nums[i]]++;
+            }
+        }
+        int max_count = 0;
+        int ans = -1;
+        for (int i = 0; i < 100000; i++) {
+            if (freq[i] > max_count) {
+                max_count = freq[i];
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
