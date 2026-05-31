f11c1450d7dddb526fa4a547f5cd2d183c5a8a30
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-05-31 11:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (96.19%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.cpp
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        vector<int> frequency(10, 0);
-        int Mul = 0;
-        if(n == 0){
-            frequency[0]++;
-        }
-        while(n > 0) {
+        int ans = 0;
+        while (n > 0) {
             int digit = n % 10;
-            frequency[digit]++;
+            ans += digit;
             n /= 10;
         }
-        for (int i = 0; i < frequency.size(); i++) {
-            Mul += frequency[i] * i;
-        }
-        return Mul;
+        return ans;
     }
 };
