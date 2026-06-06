a1f20579a28b997ec659cce886d9fab1384626f5
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-06-06 11:32:11 +0530

    Time: 3 ms (62.30%) | Memory: 11.1 MB (61.91%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        string temp;
+        int i = 1;
+        int n = path.size();
+
+        while (i < n) {
+
+            while (i < n && path[i] == '/') {
+                i++;
+            }
+
+            while (i < n && path[i] != '/') {
+                temp += path[i++];
+            }
+
+            if (temp == "..") {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } else if (temp != "" && temp != "." && temp != "..") {
+                st.push(temp);
+            }
+
+            temp.clear();
+        }
+
+        vector<string> vec;
+
+        while (!st.empty()) {
+            vec.push_back(st.top());
+            st.pop();
+        }
+
+        temp = "";
+
+        for (int i = vec.size() - 1; i >= 0; i--) {
+            temp += "/" + vec[i];
+        }
+
+        if (temp == "") {
+            temp = "/";
+        }
+
+        return temp;
+    }
+};
