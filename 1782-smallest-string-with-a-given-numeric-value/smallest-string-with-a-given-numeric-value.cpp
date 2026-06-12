00b603cc600099d359a31a970f2b8f9e12cd66f4
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-06-12 11:04:49 +0530

    Time: 2 ms (83.86%) | Memory: 24 MB (57.67%) - LeetSync

--- 1782-smallest-string-with-a-given-numeric-value/smallest-string-with-a-given-numeric-value.cpp
+++ 1782-smallest-string-with-a-given-numeric-value/smallest-string-with-a-given-numeric-value.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string getSmallestString(int n, int k) {
         string ans(n, 'a');
-        k = k - n;
+        k -= n;
         for (int i = n - 1; i >= 0; i--) {
             int add = min(25, k);
             ans[i] += add;
