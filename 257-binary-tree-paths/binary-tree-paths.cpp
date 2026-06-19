8b7de731aef45571de3530dc511815cacb4de76d
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-06-19 13:59:07 +0530

    Time: 3 ms (31.39%) | Memory: 17.7 MB (64.22%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -6,35 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     void solve(TreeNode* root, string path, vector<string>& ans) {
-        if(root == NULL) {
+        if (root == NULL) {
             return;
         }
-
         path += to_string(root->val);
-
-        // Leaf node
-        if(root->left == NULL && root->right == NULL) {
+        if (root->left == NULL && root->right == NULL) {
             ans.push_back(path);
             return;
         }
-
         path += "->";
-
         solve(root->left, path, ans);
         solve(root->right, path, ans);
     }
-
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
-
         solve(root, "", ans);
-
         return ans;
     }
 };
