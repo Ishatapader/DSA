dfdb06c78e3f5952744331027f4287e4c59bbf69
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-06-25 10:49:57 +0530

    Time: 48 ms (75.07%) | Memory: 70.9 MB (93.54%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
+    void heapify(int i, vector<int>& nums, int n) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int maxIdx = i;
+        if (left < n && nums[left] > nums[maxIdx]) {
+            maxIdx = left;
+        }
+        if (right < n && nums[right] > nums[maxIdx]) {
+            maxIdx = right;
+        }
+        if (i != maxIdx) {
+            swap(nums[maxIdx], nums[i]);
+            heapify(maxIdx, nums, n);
+        }
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(i, nums, n);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            swap(nums[0], nums[i]);
+            heapify(0, nums, i);
+        }
         return nums;
     }
 };
