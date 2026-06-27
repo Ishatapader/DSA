6d4eb22d681907c859cd1762373a5f542c6a6278
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-06-27 10:37:36 +0530

    Time: 17 ms (18.87%) | Memory: 57.1 MB (19.49%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -11,28 +11,50 @@ public:
     }
 
     vector<int> getNewsFeed(int userId) {
-        //user's tweet
-        priority_queue<pair<int, int>> pq;
-        for (auto tweet : tweets[userId]) {
-            pq.push({tweet.second, tweet.first});
-        }
+        
+        // {time, tweetId, userId, index}
+        priority_queue<vector<int>> pq;
 
-        // followees tweet
-        for (auto followe : followeeMap[userId]) {
-            for (auto tweet : tweets[followe]) {
-                pq.push({tweet.second, tweet.first});
+        if(!tweets[userId].empty()){
+            int idx = tweets[userId].size() - 1;
+            pq.push({tweets[userId][idx].second, //time 
+            tweets[userId][idx].first,//tweetId
+             userId, idx});
+        }
+        for(int followee : followeeMap[userId]){
+            if(!tweets[followee].empty()){
+            int idx = tweets[followee].size() - 1;
+            pq.push({tweets[followee][idx].second, 
+            tweets[followee][idx].first, followee, idx});
             }
         }
-        vector<int> ans;
-        while (!pq.empty() && ans.size() < 10) {
-            ans.push_back(pq.top().second);
+        vector<int>ans;
+        while(!pq.empty() && ans.size() < 10){
+            vector<int>curr = pq.top();
             pq.pop();
+        
+        int tweetId = curr[1];
+        int user = curr[2];
+        int idx = curr[3];
+        ans.push_back(tweetId);
+
+        //push provious tweet of the same user
+        idx--;
+
+        if(idx >= 0){
+            pq.push({tweets[user][idx].second,
+            tweets[user][idx].first,
+            user, idx});
+        }
         }
         return ans;
     }
 
     void follow(int followerId, int followeeId) {
+        if(followerId != followeeId){
+
         followeeMap[followerId].insert(followeeId);
+        }
     }
 
     void unfollow(int followerId, int followeeId) {
