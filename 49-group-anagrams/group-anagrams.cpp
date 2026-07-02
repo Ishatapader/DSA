aaa0fa95cf9f5709df65068f16e4e7120f0c3eef
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-07-02 16:50:30 +0530

    Time: 67 ms (11.69%) | Memory: 34.5 MB (8.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,57 +1,22 @@
-class Node {
-public:
-    unordered_map<char, Node*> children;
-    bool endOfWord;
-    vector<string> words;
-    Node() { endOfWord = false; }
-};
-class Trie {
-    Node* root;
-
-public:
-    Trie() {
-        root =
-            new Node(); // empty node for root and its endofword would be false
-    }
-
-    void insert(string key, string original) {
-
-        Node* temp = root;
-
-        for (char ch : key) {
-
-            if (temp->children.count(ch) == 0)
-                temp->children[ch] = new Node();
-
-            temp = temp->children[ch];
-        }
-
-        temp->endOfWord = true;
-        temp->words.push_back(original);
-    }
-
-    void dfsHelper(Node* root, vector<vector<string>>& ans) {
-        if (root->endOfWord) {
-            ans.push_back(root->words);
-        }
-        for (auto child : root->children) {
-            dfsHelper(child.second, ans);
-        }
-    }
-    void dfs(vector<vector<string>>& ans) { dfsHelper(root, ans); }
-};
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        Trie trie;
-        vector<vector<string>> ans;
-        for (string word : strs) {
-            string key = word;
-            sort(key.begin(), key.end());
-            trie.insert(key, word);
+        unordered_map<string, vector<string>>mp;
+        vector<vector<string>>ans;
+        for(auto &word: strs){
+            vector<int>freq(26,0);
+            for(int i = 0; i < word.size(); i++){
+                freq[word[i] - 'a']++;
+            }
+            string key = "";
+            for(int i = 0; i < freq.size(); i++){
+                key += to_string(freq[i])+"#";
+            }
+            mp[key].push_back(word);
         }
-        trie.dfs(ans);
+        for(auto &it: mp){
+            ans.push_back(it.second);
+        }
         return ans;
     }
 };
