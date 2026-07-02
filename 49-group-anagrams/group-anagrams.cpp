9e739e1228885dfc07a2b0410a57563b5dbbf9c4
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-07-02 16:38:52 +0530

    Time: 45 ms (16.84%) | Memory: 43.4 MB (5.26%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,57 @@
+class Node {
+public:
+    unordered_map<char, Node*> children;
+    bool endOfWord;
+    vector<string> words;
+    Node() { endOfWord = false; }
+};
+class Trie {
+    Node* root;
+
+public:
+    Trie() {
+        root =
+            new Node(); // empty node for root and its endofword would be false
+    }
+
+    void insert(string key, string original) {
+
+        Node* temp = root;
+
+        for (char ch : key) {
+
+            if (temp->children.count(ch) == 0)
+                temp->children[ch] = new Node();
+
+            temp = temp->children[ch];
+        }
+
+        temp->endOfWord = true;
+        temp->words.push_back(original);
+    }
+
+    void dfsHelper(Node* root, vector<vector<string>>& ans) {
+        if (root->endOfWord) {
+            ans.push_back(root->words);
+        }
+        for (auto child : root->children) {
+            dfsHelper(child.second, ans);
+        }
+    }
+    void dfs(vector<vector<string>>& ans) { dfsHelper(root, ans); }
+};
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
+        Trie trie;
         vector<vector<string>> ans;
         for (string word : strs) {
             string key = word;
             sort(key.begin(), key.end());
-            mp[key].push_back(word);
+            trie.insert(key, word);
         }
-        for (auto it : mp) {
-            ans.push_back(it.second);
-        }
+        trie.dfs(ans);
         return ans;
     }
 };
