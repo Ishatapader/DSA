abe44005380fe1cfbd35c0d1bf85081d68d427d9
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-07-03 16:50:25 +0530

    Time: 394 ms (37.75%) | Memory: 328.8 MB (28.64%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    bool hasPath(int src, int dest, vector<bool>&vis,  vector<vector<int>>& graph){
+        if(src == dest){
+            return true;
+        }
+        vis[src] = true;
+        for(int v : graph[src]){
+            if(!vis[v]){
+                if(hasPath(v, dest, vis, graph)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        //Build adjacency list
+        vector<vector<int>>graph(n);
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        vector<bool>vis(n, false);
+        return hasPath(source, destination, vis, graph);
+    }
+};
