aa0b56e5983dd48a0941c915fb7794e76175011c
Isha Tapader <140303904+Ishatapader@users.noreply.github.com>
2026-07-03 16:56:30 +0530

    Time: 409 ms (33.94%) | Memory: 328.6 MB (31.21%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-
-    bool hasPath(int src, int dest, vector<bool>&vis,  vector<vector<int>>& graph){
-        if(src == dest){
+    bool hasPath(int src, int dest, vector<bool>& vis,
+                 vector<vector<int>>& graph) {
+        if (src == dest) {
             return true;
         }
         vis[src] = true;
-        for(int v : graph[src]){
-            if(!vis[v]){
-                if(hasPath(v, dest, vis, graph)){
+        for (int v : graph[src]) {
+            if (!vis[v]) {
+                if (hasPath(v, dest, vis, graph)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        //Build adjacency list
-        vector<vector<int>>graph(n);
-        for(auto edge : edges){
+    bool validPath(int n, vector<vector<int>>& edges, int source,
+                   int destination) {
+        vector<vector<int>> graph(n);
+        for (auto edge : edges) {
             int u = edge[0];
             int v = edge[1];
             graph[u].push_back(v);
             graph[v].push_back(u);
         }
-        vector<bool>vis(n, false);
+        vector<bool> vis(n, false);
         return hasPath(source, destination, vis, graph);
     }
 };
